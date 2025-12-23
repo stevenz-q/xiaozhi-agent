f33e401bf1edc95fe5ba4f4a5f8d455b85d8bbb9
Steven_Z <Smith2005qaz@outlook.com>
2025-12-23 11:45:11 +0800

    feat: 引入阿里通用文本向量模型，集成pinecone向量数据库

---
+++ src/test/java/com/yqz/xiaozhi/EmbeddingTest.java
@@ -0,0 +1,45 @@
+package com.yqz.xiaozhi;
+
+import dev.langchain4j.data.embedding.Embedding;
+import dev.langchain4j.data.segment.TextSegment;
+import dev.langchain4j.model.embedding.EmbeddingModel;
+import dev.langchain4j.model.output.Response;
+import dev.langchain4j.store.embedding.EmbeddingStore;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EmbeddingTest {
+
+    @Autowired
+    private EmbeddingModel embeddingModel;
+
+    @Test
+    public void testEmbeddingModel() {
+        Response<Embedding> embed = embeddingModel.embed("你好");
+        System.out.println("向量维度：" + embed.content().vector().length);
+        System.out.println("向量输出：" + embed.toString());
+    }
+
+    @Autowired
+    private EmbeddingStore embeddingStore;
+
+    /**
+     * 将文本转换成向量，然后存储到pinecone中
+     * <p>
+     * 参考：
+     * https://docs.langchain4j.dev/tutorials/embedding-stores
+     */
+    @Test
+    public void testPineconeEmbeded() {
+        // 将文本转换成向量
+        TextSegment segment1 = TextSegment.from("我喜欢羽毛球");
+        Embedding embedding1 = embeddingModel.embed(segment1).content();
+        // 存入向量数据库
+        embeddingStore.add(embedding1, segment1);
+        TextSegment segment2 = TextSegment.from("今天天气很好");
+        Embedding embedding2 = embeddingModel.embed(segment2).content();
+        embeddingStore.add(embedding2, segment2);
+    }
+}
